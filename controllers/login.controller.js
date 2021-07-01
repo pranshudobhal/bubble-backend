df2cfdb83682f6b21bda0ec3b723227260c084bf
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-07-02 01:00:57 +0530

    setup authentication

---
+++ controllers/login.controller.js
@@ -0,0 +1,31 @@
+const { User } = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const loginUser = async (req, res) => {
+  try {
+    const { userNameOrEmail, password } = req.body;
+
+    const usernameCheck = await User.findOne({ username: userNameOrEmail });
+    const emailCheck = await User.findOne({ email: userNameOrEmail });
+
+    const user = usernameCheck || emailCheck;
+
+    if (!user) {
+      return res.status(401).json({ success: false, message: 'No such user exists!!!' });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(401).json({ success: false, message: 'Error logging in!!!' });
+    }
+
+    const token = jwt.sign({ userID: user._id }, process.env.SECRET, { expiresIn: '24h' });
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    res.json({ success: false, message: 'Some error with login!' });
+  }
+};
+
+module.exports = { loginUser };
---
+++ controllers/signup.controller.js
@@ -0,0 +1,39 @@
+const { User } = require('../models/user.model');
+
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const createNewUser = async (req, res) => {
+  console.log('here in signup');
+  try {
+    const { firstName, lastName, username, email, password } = req.body;
+    const saltRounds = 10;
+    const user = await User.find({ email });
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    if (user.length >= 1) {
+      return res.status(409).json({ success: false, message: 'User already exists' });
+    } else {
+      const newUser = new User({
+        _id: new mongoose.Types.ObjectId(),
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        email: email,
+        password: hashedPassword,
+      });
+
+      const token = jwt.sign({ userID: newUser._id }, process.env.SECRET, { expiresIn: '24h' });
+
+      await newUser.save();
+
+      res.status(201).json({ success: true, message: 'User has been created', token });
+    }
+  } catch (error) {
+    res.json({ success: false, message: 'Error creating new user!', errorMessage: error.message });
+  }
+};
+
+module.exports = { createNewUser };
